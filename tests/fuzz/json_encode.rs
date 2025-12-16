6ff118b71cac5dfecc41334e9436c7f0ed3bad36
Lee Dogeon <dev.moreal@gmail.com>
2025-12-16 12:25:03 +0900

    test: fuzz with proptest for preserving regressions

--- tests/fuzz/json_encode.rs
+++ tests/fuzz/json_encode.rs
@@ -1,18 +1,20 @@
-use super::ArbitraryBencodexValue;
+use super::bencodex_value;
 use bencodex::json::{to_json, to_json_with_options, BinaryEncoding, JsonEncodeOptions};
-use quickcheck_macros::quickcheck;
+use proptest::prelude::*;
 
-#[quickcheck]
-fn json_encode_produces_valid_json(value: ArbitraryBencodexValue) -> bool {
-    let json_str = to_json(&value.0);
-    serde_json::from_str::<serde_json::Value>(&json_str).is_ok()
-}
+proptest! {
+    #[test]
+    fn json_encode_produces_valid_json(value in bencodex_value()) {
+        let json_str = to_json(&value);
+        prop_assert!(serde_json::from_str::<serde_json::Value>(&json_str).is_ok());
+    }
 
-#[quickcheck]
-fn json_encode_hex_produces_valid_json(value: ArbitraryBencodexValue) -> bool {
-    let options = JsonEncodeOptions {
-        binary_encoding: BinaryEncoding::Hex,
-    };
-    let json_str = to_json_with_options(&value.0, options);
-    serde_json::from_str::<serde_json::Value>(&json_str).is_ok()
+    #[test]
+    fn json_encode_hex_produces_valid_json(value in bencodex_value()) {
+        let options = JsonEncodeOptions {
+            binary_encoding: BinaryEncoding::Hex,
+        };
+        let json_str = to_json_with_options(&value, options);
+        prop_assert!(serde_json::from_str::<serde_json::Value>(&json_str).is_ok());
+    }
 }
